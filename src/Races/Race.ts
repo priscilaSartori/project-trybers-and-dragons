0866ff272912941f60473259c779ac9aa84fd108
Priscila <priscilasartori2@gmail.com>
2023-03-27 16:25:59 -0300

    feat: adicionando atributos e construtor da classe Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,9 @@
-export default abstract class Race {}
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+}
