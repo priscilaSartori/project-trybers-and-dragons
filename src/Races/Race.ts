3fcdefec848f4c788457889c17df538504fa5189
Priscila <priscilasartori2@gmail.com>
2023-03-27 16:27:01 -0300

    feat: Implementando a leitura dos atributos

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -6,4 +6,12 @@ export default abstract class Race {
     this._name = name;
     this._dexterity = dexterity;
   }
+
+  public get name(): string {
+    return this._name;
+  }
+  
+  public get dexterity(): number {
+    return this._dexterity;
+  }
 }
