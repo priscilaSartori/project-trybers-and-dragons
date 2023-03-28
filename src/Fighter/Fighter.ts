9c2f017e8f12c4adef73143ff8a9396de9aa74ee
Priscila <priscilasartori2@gmail.com>
2023-03-28 00:42:44 -0300

    feat: Implementando os métodos da interface Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -5,4 +5,9 @@ export default interface Fighter {
   strength: number;
   defense: number;
   energy?: Energy;
+
+  attack(enemy: Fighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
 }
