b4eb7a7f394d0115f9806d3057e644ae28a09a43
Priscila <priscilasartori2@gmail.com>
2023-03-28 19:14:55 -0300

    feat: Implementando a leitura dos atributos

--- src/Monster.ts
+++ src/Monster.ts
@@ -3,9 +3,17 @@ import { SimpleFighter } from './Fighter';
 export default class Monster implements SimpleFighter {
   private _lifePoints: number;
   private _strength: number;
-
+  
   constructor() {
     this._lifePoints = 85;
     this._strength = 63;
+  }
+  
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+  
+  public get strength(): number {
+    return this._strength;
   }
 }
