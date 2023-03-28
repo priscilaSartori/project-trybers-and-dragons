9989cf0e3e34ca2ccbaba629b05904b5fbc81f88
Priscila <priscilasartori2@gmail.com>
2023-03-27 23:46:37 -0300

    feat: adicionando atributos e construtor da classe Archetypes

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,1 +1,11 @@
-export default abstract class Archetypes {}
+export default abstract class Archetypes {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+}
