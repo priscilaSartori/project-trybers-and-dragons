40c7328266424f5aeaee169457a877ba8c9c5d31
Priscila <priscilasartori2@gmail.com>
2023-03-28 00:15:08 -0300

    feat: Implementando os atributos e métodos da classe Warrior

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,5 +1,21 @@
 import Archetypes from './Archetype';
+import { EnergyType } from '../Energy';
 
 export default class Warrior extends Archetypes {
+  private _energyType: EnergyType;
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
   
+  static createdRacesInstances(): number {
+    return Warrior._instances;
+  }
 }
