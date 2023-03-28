cfc56de4ae653784275cbc10cf838c3033182b6c
Priscila <priscilasartori2@gmail.com>
2023-03-28 00:50:58 -0300

    feat: Implementando os atributos da classe Character

--- src/Character.ts
+++ src/Character.ts
@@ -1,5 +1,15 @@
 import Fighter from './Fighter';
+import Race from './Races';
+import Archetype from './Archetypes';
+import Energy from './Energy';
 
 export default class Character implements Fighter {
-
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
 }
