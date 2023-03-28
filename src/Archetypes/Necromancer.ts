e27c5d42e0a8534592fa746023e224a0168ce790
Priscila <priscilasartori2@gmail.com>
2023-03-28 00:14:06 -0300

    feat: Implementando os atributos e métodos da classe Necromancer

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,5 +1,21 @@
 import Archetypes from './Archetype';
+import { EnergyType } from '../Energy';
 
 export default class Necromancer extends Archetypes {
+  private _energyType: EnergyType;
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer._instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
   
+  static createdRacesInstances(): number {
+    return Necromancer._instances;
+  }
 }
