ecfd1c3e5d206e2aa75908cdca318d88c4d1cc01
Priscila <priscilasartori2@gmail.com>
2023-03-27 22:31:38 -0300

    feat: Implementando os atributos e métodos da classe Orc

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,5 +1,20 @@
 import Race from "./Race";
 
 export default class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Orc._instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
   
+  static createdRacesInstances(): number {
+    return Orc._instances;
+  }
 }
