914c2e288906e4e2c44fcf7da578a47b7b2e5433
Priscila <priscilasartori2@gmail.com>
2023-03-27 22:29:13 -0300

    feat: Implementando os atributos e métodos da classe Dwarf

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,5 +1,20 @@
 import Race from "./Race";
 
 export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _instances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
   
+  static createdRacesInstances(): number {
+    return Dwarf._instances;
+  }  
 }
