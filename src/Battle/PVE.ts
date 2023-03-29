0760402e591e2b4be066bc622184584191d682ee
Priscila <priscilasartori2@gmail.com>
2023-03-28 21:29:02 -0300

    feat: adicionando atributos e construtor da classe PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,3 +1,12 @@
 import Battle from './Battle';
+import Fighter, { SimpleFighter } from '../Fighter';
 
-export default class PVE extends Battle {}
+export default class PVE extends Battle {
+  private _character: Fighter;
+  private _monsters: [Fighter | SimpleFighter];
+  constructor(character: Fighter, monster: [Fighter | SimpleFighter]) { 
+    super(character);
+    this._character = character;
+    this._monsters = monster; 
+  }
+}
