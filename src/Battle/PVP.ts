0856f6897b8dcbcb89be43bc6a420d46533b6be0
Priscila <priscilasartori2@gmail.com>
2023-03-28 21:09:47 -0300

    feat: adicionando o metodo fight da classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -9,4 +9,12 @@ export default class PVP extends Battle {
     this._character1 = character1;
     this._character2 = character2; 
   }
+
+  fight(): number {
+    while (this._character1.lifePoints > 0 && this._character2.lifePoints > 0) {
+      this._character1.attack(this._character2);
+      this._character2.attack(this._character1);
+    }
+    return super.fight();
+  }
 }
