836df792829d484ef9d53ac855a1eac5b350ac0a
Priscila <priscilasartori2@gmail.com>
2023-03-29 11:51:55 -0300

    fix: Alterando array de monsters

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -3,8 +3,8 @@ import Fighter, { SimpleFighter } from '../Fighter';
 
 export default class PVE extends Battle {
   private _character: Fighter;
-  private _monsters: [Fighter | SimpleFighter];
-  constructor(character: Fighter, monster: [Fighter | SimpleFighter]) { 
+  private _monsters: Fighter[] | SimpleFighter[];
+  constructor(character: Fighter, monster: Fighter[] | SimpleFighter[]) { 
     super(character);
     this._character = character;
     this._monsters = monster; 
