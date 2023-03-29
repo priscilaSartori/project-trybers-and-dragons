c6c7e50b30c718d9efbac8c58a9e321952cdc6bf
Priscila <priscilasartori2@gmail.com>
2023-03-28 21:08:47 -0300

    feat: adicionando atributos e construtor da classe PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,12 @@
 import Battle from './Battle';
+import Fighter from '../Fighter';
 
 export default class PVP extends Battle {
-  
+  private _character1: Fighter;
+  private _character2: Fighter;
+  constructor(character1: Fighter, character2: Fighter) { 
+    super(character1);
+    this._character1 = character1;
+    this._character2 = character2; 
+  }
 }
